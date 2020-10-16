8e760ff611c87e2ee92ce44e560fcf938eca5f2b
Joe Zhang <jozhang123@gmail.com>
2020-10-16 04:11:31 -0700

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Route } from 'react-router-dom'
+import Home from './components/Home';
+import Shop from './components/Shop';
 
 function App() {
   return (
-    <div className="App">
-       
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Route exact path='/' component={Home} />
+        <Route path='/shop' component={Shop} />
+      </div>
+    </BrowserRouter>
   );
 }
 
